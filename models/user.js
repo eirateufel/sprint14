06a1072c0f921910832d639ed13e2290d6f5c687
Eira Teufel <sv.tolstyakova@gmail.com>
2020-04-03 19:03:52 +0300

    +findUser method

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
 	name: {
@@ -40,8 +41,26 @@ const userSchema = new mongoose.Schema({
 	},
 	password: {
 		type: String,
-		required: true,
+    required: true,
+    select: false,
 	},
 });
+
+userSchema.statics.findUser = function (email, password) {
+	return this.findOne({ email }).select('+password')
+		.then((user) => {
+			if (!user) {
+				return Promise.reject(new Error('Неправильные почта или пароль'));
+			}
+
+			return bcrypt.compare(password, user.password)
+				.then((matched) => {
+					if (!matched) {
+						return Promise.reject(new Error('Неправильные почта или пароль'));
+					}
+					return user;
+				});
+		});
+};
 
 module.exports = mongoose.model('user', userSchema);
