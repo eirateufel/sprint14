512a724fd373eefcf3c2c3263468859f2b39bfee
Eira Teufel <sv.tolstyakova@gmail.com>
2020-04-03 14:47:38 +0300

    createUser modified

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
@@ -14,15 +15,21 @@ module.exports.getUser = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-	const { name, about, avatar } = req.body;
-
-	User.create({ name, about, avatar })
+	const {
+		name, about, avatar, email,
+	} = req.body;
+	bcrypt.hash(req.body.password, 10)
+		.then((hash) => User.create({
+			name, about, avatar, email, password: hash,
+		}))
 		.then((user) => res.send({ data: user }))
 		.catch((err) => {
 			if (err.name === 'ValidationError') {
 				res.status(400).send({ message: `Введенные данные некорректны: ${err.message}` });
+			} else if (err.message.includes('E11000 duplicate key error')) {
+				res.status(500).send({ message: 'Пользователь с таким имейлом уже зарегистрирован' });
 			} else {
-				res.status(500).send({ message: err });
+				res.status(500).send({ message: err.message });
 			}
 		});
 };
